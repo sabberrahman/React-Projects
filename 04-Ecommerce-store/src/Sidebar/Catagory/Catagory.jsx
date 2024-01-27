6a29572342d008f2f738789cfbd71cae30108a47
sabberrahman <sabbersr1@gmail.com>
2024-01-27 23:09:31 +0600

    added category

--- 04-Ecommerce-store/src/Sidebar/Catagory/Catagory.jsx
+++ 04-Ecommerce-store/src/Sidebar/Catagory/Catagory.jsx
@@ -3,8 +3,26 @@ import "./Catagory.css"
 
 const Catagory = () => {
     return (
-        <div>
-            bhaii Catagory
+        <div >
+           <h2 className='sidebar-title'>Category</h2>
+
+           <label className="sidebar-label-container">
+            <input type="radio" name="test"/>
+            <span className="checkmark">All</span>
+           </label>
+           <label className="sidebar-label-container">
+            <input type="radio" name="test"/>
+            <span className="checkmark">sneakers</span>
+           </label>
+           <label className="sidebar-label-container">
+            <input type="radio" name="test"/>
+            <span className="checkmark">Flats</span>
+           </label>
+           <label className="sidebar-label-container">
+            <input type="radio" name="test"/>
+            <span className="checkmark">Heels</span>
+           </label>
+
         </div>
     );
 };
