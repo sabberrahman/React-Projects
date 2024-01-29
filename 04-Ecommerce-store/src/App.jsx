1e09e4162642e4d7852fc4418c833f7bb41e0e20
sabberrahman <sabbersr1@gmail.com>
2024-01-29 07:29:12 +0600

    logic

--- 04-Ecommerce-store/src/App.jsx
+++ 04-Ecommerce-store/src/App.jsx
@@ -1,14 +1,54 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navigation from './Navigation/Nav'
 import Products from './Products/Products'
 import Recommended from './Recommended/Recommended'
 import Sidebar from './Sidebar/Sidebar'
 
+//database 
+import products from "./Dataa/Data"
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedCategory, setselectedCategory] = useState(null)
+
+  //input filter 
+  const [query,setQuery]=useState("")
+
+  const handleInputChange = (e)=>{
+     setQuery(e.target.value)
+  }
+
+   // Radio Filtering 
+  const handleChange = (event) => {
+    setselectedCategory(event.target.value);
+  };
+
+   // Button Filtering 
+  const handleClick = (event) => {
+    setselectedCategory(event.target.value);
+  };
+
+  function filteredData(products, selected, query) {
+    let filteredProducts = products;
+
+    // Filtering Input Items
+    if (query) {
+      filteredProducts = filteredItems;
+    }
+
+    // Applying selected filter
+    if (selected) {
+      filteredProducts = filteredProducts.filter(
+        ({ category, color, company, newPrice, title }) =>
+          category === selected ||
+          color === selected ||
+          company === selected ||
+          newPrice === selected ||
+          title === selected
+      );
+    }
+
+   
 
   return (
     <>
